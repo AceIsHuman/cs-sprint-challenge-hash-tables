52efdd51ae53a28bab4321e7820feda4ba854fe1
AceIsHuman <menoman1995@yahoo.com>
2020-07-18 13:35:38 -0500

    use linkedList to store paths

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -10,7 +10,16 @@ def finder(paths, queries):
         path_split = path.split('/')
         name_index = len(path_split) - 1
         file_name = path_split[name_index]
-        file_paths[file_name] = path
+
+        if file_paths.get(file_name) is not None:
+            node = file_paths[file_name]
+            while node.next is not None:
+                node = node.next
+            node.next = LinkedList(path)
+        else:
+            file_paths[file_name] = LinkedList(path)
+    
+
 
     return result
 
